5fda41de36174e1090e9322d533fb09f5816a503
Tom Switzer <thomas.switzer@gmail.com>
2018-10-20 01:48:02 +0000

    Add more tests for Vectors.

--- core/src/test/scala/net/tixxit/gulon/VectorsSpec.scala
+++ core/src/test/scala/net/tixxit/gulon/VectorsSpec.scala
@@ -22,7 +22,6 @@ class VectorsSpec extends FunSuite with PropertyChecks {
 
   val genSubVectors: Gen[Seq[Vectors]] = for {
     matrix <- genMatrix
-    _ = if (matrix.cols == 0) println("wtf") else ()
     n <- Gen.choose(1, matrix.cols)
   } yield Vectors.subvectors(matrix, n)
 
@@ -37,6 +36,30 @@ class VectorsSpec extends FunSuite with PropertyChecks {
   test("slice()") {
     forAll { (vectors: Vectors) =>
       assert(vectors == Vectors(vectors.slice()))
+    }
+  }
+
+  test("subvectors splits data") {
+    val genArgs: Gen[(Matrix, Int)] = for {
+      matrix <- genMatrix
+      n <- Gen.choose(1, matrix.cols)
+    } yield (matrix, n)
+
+    forAll(genArgs) { case (matrix, n) =>
+      val subvectors = Vectors.subvectors(matrix, n)
+      assert(subvectors.length == n)
+      val maxSize = subvectors.map(_.dimension).max
+      val minSize = subvectors.map(_.dimension).min
+      assert((maxSize - minSize) <= 1)
+      subvectors.foreach { vectors =>
+        assert(vectors.size == matrix.rows)
+        val slice = Vectors(Matrix(
+          vectors.size, vectors.dimension,
+          matrix.data.map { row =>
+            Array.range(vectors.from, vectors.until).map(row(_))
+          }))
+        assert(slice == vectors)
+      }
     }
   }
 }
