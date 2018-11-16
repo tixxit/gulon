2f659e4717ca4cf745ee742b11bf7b971ad0ada9
Tom Switzer <thomas.switzer@gmail.com>
2018-11-16 04:32:31 +0000

    Test TopKHeap#merge.

--- core/src/test/scala/net/tixxit/gulon/TopKHeapSpec.scala
+++ core/src/test/scala/net/tixxit/gulon/TopKHeapSpec.scala
@@ -29,4 +29,25 @@ class TopKHeapSpec extends FunSuite with PropertyChecks {
       assert(actual == expected)
     }
   }
+
+  test("merge") {
+    val genArgs = for {
+      kvs <- Arbitrary.arbitrary[List[List[(Int, Float)]]]
+      k <- Gen.choose(1, math.max(2 * kvs.map(_.size).sum, 1))
+    } yield (kvs, k)
+
+    forAll(genArgs) { case (kvs, k) =>
+      val heap = TopKHeap(k)
+      val heaps = kvs.map { data =>
+        val heap0 = TopKHeap(k)
+        data.foreach { case (i, x) =>
+          heap0.update(i, x)
+        }
+        heap.merge(heap0)
+      }
+      val actual = toVector(heap)
+      val expected = kvs.flatten.sortBy(_._2).take(k)
+      assert(actual == expected)
+    }
+  }
 }
